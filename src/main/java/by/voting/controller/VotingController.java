80275f5cd14d194b7324a3fb9f354a4fbaca9122
Lino <Libo220037@gmail.com>
2018-04-01 00:38:17 +0300

    add home page

--- src/main/java/by/voting/controller/VotingController.java
+++ src/main/java/by/voting/controller/VotingController.java
@@ -1,11 +1,17 @@
 package by.voting.controller;
 
 
+import by.voting.entity.Vote;
+import by.voting.entity.Voting;
 import by.voting.service.interfaceService.VoteService;
 import by.voting.service.interfaceService.VotingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class VotingController {
@@ -19,8 +25,33 @@ public class VotingController {
         this.votingService = votingService;
     }
 
+    @ModelAttribute("vote")
+    public Vote vote(){
+        return new Vote();
+    }
+
+    @ModelAttribute("voting")
+    public Voting voting(){
+        return new Voting();
+    }
+
     @GetMapping("/")
-    public String homePageGet (){
+    public String homePageGet () {
         return "homePage";
+    }
+
+    @PostMapping("/")
+    public String homePagePost (Voting voting, Model model){
+        votingService.save(voting);
+        model.addAttribute("id", voting.getId());
+        return "redirect:/voting/{id}";
+    }
+
+    @GetMapping("/voting/{id}")
+    public String votingAddGet (@PathVariable("id") Long id, Vote vote){
+        Voting votingById = votingService.findById(id).get();
+        vote.setVoting(votingById);
+        voteService.save(vote);
+        return "redirect:/";
     }
 }
